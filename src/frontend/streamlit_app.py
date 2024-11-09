397fdfbd26b73ac8e34e80ef271a390a3a8c7c66
Yong Chein <yongchein.o@gmail.com>
2024-11-09 04:04:56 +0000

    Added Chatbot

--- src/frontend/streamlit_app.py
+++ src/frontend/streamlit_app.py
@@ -1,27 +1,28 @@
 import streamlit as st
 import datetime
 
+
+# ============================================================
+# Configuration and Initialisation
+# ============================================================
+
+# Confiugure the app to have a wide layout
 st.set_page_config(
-    page_title="My App", 
-    layout="wide", 
+    layout="wide",
     # initial_sidebar_state="collapsed"
 )
-
-SELECTIONS = [
-    "Business Intelligence",
-    "Anomaly Detection", 
-    "ASIC Reporting",
-    "Future Functionality 1", 
-    "Future Functionality 2",
-]
 
 # Initialize selections as an empty list in session state
 if "selections" not in st.session_state:
     st.session_state.selections = []
+
 # Initialize date as None in session state
 if "date" not in st.session_state:
     st.session_state.date = None
 
+
+# Update the style of multiselect input so that selected options will have
+# their text shown in full
 def update_multiselect_style():
     st.markdown(
         """
@@ -36,15 +37,32 @@ def update_multiselect_style():
         """,
         unsafe_allow_html=True,
     )
+
+
 update_multiselect_style()
 
 
+# ============================================================
+# Adding Pages and Routing
+# ============================================================
+
+
+# Define the log in page for user to select the date and functionalities
 def login():
-    st.header("Dashboard View Customization")
+    SELECTIONS = [
+        "Business Intelligence",
+        "Anomaly Detection",
+        "ASIC Reporting",
+        "<Future Functionality 1>",
+        "<Future Functionality 2>",
+    ]
+    st.header("IRESS End of Day Reporting Dashboard View Customization")
     today = datetime.datetime.now()
-    date = st.date_input("Select the date for the report", max_value = today, value=None)
+    date = st.date_input(
+        "Which date's data would you like to view?", max_value=today, value=None
+    )
     selections = st.multiselect(
-        "Select the desired dashboard",
+        "Which functinalities and charts would you like to view?",
         SELECTIONS,
         placeholder="Nothing selected yet",
     )
@@ -54,25 +72,29 @@ def login():
         st.rerun()
 
 
+# Define the log out page and clear the session_state
 def logout():
     st.session_state.selections = []
     st.session_state.date = None
+    # Reset the chatbot
+    st.cache_resource.clear()
+    st.session_state.messages = []
     st.rerun()
 
-# Declare selections and conn
+
+# Retrieve current session_state
 selections = st.session_state.selections
 date = st.session_state.date
 
-# conn = st.connection("my_database")
-# df = conn.query("select * from trial")
-# st.dataframe(df)
-
+# Add logout page to st.Page
 logout_page = st.Page(
     logout,
     title="Return to Chart Selections",
     icon=":material/logout:",
     default=(not bool(selections) or not bool(selections)),
 )
+
+# Add dashboard page to st.Page
 dashboard_page = st.Page(
     "dashboard.py",
     title="Current Dashboard",
@@ -80,15 +102,23 @@ dashboard_page = st.Page(
     default=(bool(selections) and bool(date)),
 )
 
+# Add chatbot page to st.Page
+chatbot_page = st.Page(
+    "../llm/chatbot/st_integration2.py",
+    title="Ask Chatbot Ad-Hoc Queries",
+    icon=":material/robot_2:",
+    default=False,
+)
 
-# Define common element
-# st.title("Capstone Project: End-of-Day Summary Dashboard")
+# Routing between different pages
+# Only display the dashboard and chatbot pages when selections for dates and charts are made
 if selections and (date is not None):
     # Display logout option if selections are made
     pg = st.navigation(
         {
             "": [
                 dashboard_page,
+                chatbot_page,
                 logout_page,
             ]
         }
@@ -96,7 +126,6 @@ if selections and (date is not None):
 else:
     # Display login if no selections
     pg = st.navigation([st.Page(login)])
-
 
 # Run the navigation
 pg.run()
